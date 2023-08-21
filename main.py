800d1057a44d8dec6983acc5997705dcd4d3a8a7
Rao <103417697+Aby-ss@users.noreply.github.com>
2023-08-21 16:29:06 +0500

    Added base layout test / design ✅

--- main.py
+++ main.py
@@ -30,3 +30,37 @@ class Footer(Placeholder):
         dock: bottom;
     }
     """
+
+class ConsoleTrader(App):
+    CSS_PATH = "layout.css"
+
+    def compose(self) -> ComposeResult:
+    
+        chart_text = "test"
+        company_overview_text = "test"
+        balance_sheet_text = "test"
+        cash_flow_text = "test"
+        income_statement_text = "test"
+        
+        yield Header("ConsoleTrader", classes="Header",)
+        yield Footer("Empowering Investments, Simplifying Decisions!")
+        yield Horizontal(
+            Vertical(
+                Static(f"{chart_text}"),
+                Static(f"{company_overview_text}"),
+                classes="column",
+            ),
+            Vertical(
+                Static(f"{balance_sheet_text}"),
+                Static(f"{cash_flow_variables_text}"),
+                Static(f"{income_statement_variables_text}"),
+                classes="column",
+            ),
+        )
+    
+        def on_mount(self) -> None:
+            self.Header.styles.background = "#9932CC"
+
+if __name__ == "__main__":
+    app = ConsoleTrader()
+    app.run()
