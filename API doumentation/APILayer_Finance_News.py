685fb40e71c49a77880919b2e0eba88d690d7fc2
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-08-27 16:18:10 +0400

    Added Rich Panels for pretty printing ✅

--- API doumentation/APILayer_Finance_News.py
+++ API doumentation/APILayer_Finance_News.py
@@ -1,14 +1,29 @@
 import requests
+from rich import print
+from rich.panel import Panel
 
 url = "https://api.apilayer.com/financelayer/news?apikey=EHw0Ndjb26EG0xvAK5uNSO5oNkgp5w73&tickers=appl&tickers=btc&tags=Oil&sources=forbes.com&sort=desc&offset=5&limit=5&keywords=oil%20prices&fallback=on&date=thismonth"
 
-payload = {}
+response = requests.get(url)
+data = response.json()
 
-response = requests.request("GET", url, data=payload)
+news_objects = data["data"]
 
-status_code = response.status_code
-result = response.text
-
-print("Response Status Code:", status_code)
-print("Response Content:")
-print(result)
+for news in news_objects:
+    title = news["title"]
+    description = news["description"]
+    url = news["url"]
+    source = news["source"]
+    published_at = news["published_at"]
+    
+    panel = Panel.fit(
+        
+        f"Description: {description}\n"
+        f"Source: {source}\n"
+        f"Published At: {published_at}",
+        title=f"[link={url}]Title:[/link] {title}\n",
+        border_style="green"
+    )
+    
+    print(panel)
+    print()  # Add an empty line between panels
