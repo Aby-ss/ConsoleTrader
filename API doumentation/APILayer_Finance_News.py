4e71c7cf8e5bc42e87a31be244545d7325a5e2ec
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-08-27 16:21:42 +0400

    Updated Panel looks 👔

--- API doumentation/APILayer_Finance_News.py
+++ API doumentation/APILayer_Finance_News.py
@@ -1,6 +1,9 @@
 import requests
-from rich import print
+from rich import print, box
 from rich.panel import Panel
+from rich.layout import Layout
+from rich.traceback import install
+install(show_locals=True)
 
 url = "https://api.apilayer.com/financelayer/news?apikey=EHw0Ndjb26EG0xvAK5uNSO5oNkgp5w73&tickers=appl&tickers=btc&tags=Oil&sources=forbes.com&sort=desc&offset=5&limit=5&keywords=oil%20prices&fallback=on&date=thismonth"
 
@@ -16,13 +19,15 @@ for news in news_objects:
     source = news["source"]
     published_at = news["published_at"]
     
-    panel = Panel.fit(
+    panel = Panel(
         
         f"Description: {description}\n"
         f"Source: {source}\n"
         f"Published At: {published_at}",
         title=f"[link={url}]Title:[/link] {title}\n",
-        border_style="green"
+        title_align="left",
+        border_style="bold white",
+        box=box.SQUARE
     )
     
     print(panel)
