db63da5dc2f5dea35fe5c214fbe078075dac1793
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-08-27 16:06:00 +0400

    Updated Base Finance News code ✅

---
+++ API doumentation/APILayer_Finance_News.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://api.apilayer.com/financelayer/news?apikey=EHw0Ndjb26EG0xvAK5uNSO5oNkgp5w73&tickers=appl&tickers=btc&tags=Oil&sources=forbes.com&sort=desc&offset=5&limit=5&keywords=oil%20prices&fallback=on&date=thismonth"
+
+payload = {}
+
+response = requests.request("GET", url, data=payload)
+
+status_code = response.status_code
+result = response.text
