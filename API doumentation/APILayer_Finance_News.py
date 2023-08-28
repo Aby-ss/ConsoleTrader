9a43577a3417b3249b31df0e487c97199e873f64
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-08-28 13:17:45 +0400

    Added News to Dashboard ✅

--- API doumentation/APILayer_Finance_News.py
+++ API doumentation/APILayer_Finance_News.py
@@ -5,41 +5,39 @@ from rich.layout import Layout
 from rich.traceback import install
 install(show_locals=True)
 
-def news_APILayer():
+url = "https://api.apilayer.com/financelayer/news?apikey=EHw0Ndjb26EG0xvAK5uNSO5oNkgp5w73&tickers=appl&tickers=btc&tags=Oil&sources=forbes.com&sort=desc&offset=5&limit=7&keywords=oil%20prices&fallback=on&date=thismonth"
 
-    url = "https://api.apilayer.com/financelayer/news?apikey=EHw0Ndjb26EG0xvAK5uNSO5oNkgp5w73&tickers=appl&tickers=btc&tags=Oil&sources=forbes.com&sort=desc&offset=5&limit=7&keywords=oil%20prices&fallback=on&date=thismonth"
+response = requests.get(url)
+data = response.json()
 
-    response = requests.get(url)
-    data = response.json()
+news_objects = data["data"]
 
-    news_objects = data["data"]
+# Create a layout to organize panels
+layout = Layout(name="root")
 
-    # Create a layout to organize panels
-    layout = Layout(name="root")
+# Create a single panel to hold all news articles
+news_panel_content = ""
 
-    # Create a single panel to hold all news articles
-    news_panel_content = ""
+for news in news_objects:
+    title = news["title"]
+    description = news["description"]
+    url = news["url"]
+    source = news["source"]
+    published_at = news["published_at"]
+    
+    # Add each news article's information to the aggregated content
+    news_panel_content += (
+        f"Title: [link={url}]{title}[/link]\n"
+        f"Description: {description}\n"
+        f"Source: {source}\n"
+        f"Published At: {published_at}\n\n"
+    )
 
-    for news in news_objects:
-        title = news["title"]
-        description = news["description"]
-        url = news["url"]
-        source = news["source"]
-        published_at = news["published_at"]
-        
-        # Add each news article's information to the aggregated content
-        news_panel_content += (
-            f"Title: [link={url}]{title}[/link]\n"
-            f"Description: {description}\n"
-            f"Source: {source}\n"
-            f"Published At: {published_at}\n\n"
-        )
+# Create the panel with aggregated content
+news_panel = Panel(news_panel_content, title="Aggregated News", border_style="bold white", box=box.SQUARE)
 
-    # Create the panel with aggregated content
-    news_panel = Panel(news_panel_content, title="Aggregated News", border_style="bold white", box=box.SQUARE)
+# Add the news panel to the layout
+layout.split_column(news_panel)
 
-    # Add the news panel to the layout
-    layout.split_column(news_panel)
-
-    # Print the layout to display the news panel
-    return layout
+# Print the layout to display the news panel
+return layout
