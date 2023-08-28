aac4b3be0d420767c3bffdfeb97bfa527d04fc3b
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-08-28 12:45:25 +0400

    Added rich dashboard file ✅

---
+++ Main Dashboard/rich_dashboard.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import csv
+import time
+from time import sleep
+import math
+import keyboard
+import numpy as np
+import asciichartpy
+
+from rich import print
+from rich import box
+from rich.tree import Tree
+from rich.text import Text
+from rich.align import Align
+from rich.panel import Panel
+from rich.layout import Layout 
+from rich.table import Table
+
+from rich.live import Live
+from rich.prompt import Prompt
+from rich.progress import track
+from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn
+
+from rich.traceback import install
+install(show_locals=True)
+
+layout = Layout()
+
+layout.split_column(
+    Layout(name = "Header", size=3),
+    Layout(name = "Body"),
+    Layout(name = "Footer", size=3)
+)
+
+layout["Body"].split_row(
+    Layout(name="Right"), # Use Right side for Weather Analytics and News
+    Layout(name="Left")
+)
+
+
+
+class Header:
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="left")
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row(
+            "🗃", "[b]Ultimate Financial Informative System[/]", datetime.now().ctime().replace(":", "[blink]:[/]"),
+        )
+        return Panel(grid, style="bold white")
+    
+class Footer:
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="center", ratio=1)
+        grid.add_row("[i]Empowering Investments, Simplifying Decisions[/]")
+        return Panel(grid, style="white on black")
+
+    
+    
+layout["Header"].update(Header())
+layout["Footer"].update(Footer())
+
+print(layout)
